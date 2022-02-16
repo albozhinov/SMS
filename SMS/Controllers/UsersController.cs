5d8e192275dc5d7a612d48be7127836100981d2b
albozhinov <a.bozhinov90@gmail.com>
2022-02-16 20:47:55 +0200

    UserController and RegisterUserViewModel added. In HomeController implemented Index logic.

---
+++ SMS/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿namespace SMS.Controllers
+{
+    using BasicWebServer.Server.Attributes;
+    using BasicWebServer.Server.Controllers;
+    using BasicWebServer.Server.HTTP;
+    using SMS.Services.Contracts;
+
+    public class UsersController : Controller
+    {
+        private readonly IUserService userService; 
+
+        public UsersController(Request request, IUserService _userService)
+            : base(request)
+        {
+            userService = _userService;
+        }
+
+        [HttpGet]
+        public Response Login()
+        {
+            if (User.IsAuthenticated)
+            {
+                return Redirect("/");
+            }
+
+            return View(new { IsAuthenticated = false});
+        }
+
+        [HttpGet]
+        public Response Register()
+        {
+            if (User.IsAuthenticated)
+            {
+                return Redirect("/");
+            }
+
+            return View(new { IsAuthenticated = false });
+        }
+
+        [HttpPost]
+        public Response Register(RegisterUserViewModel model)
+        {
+            
+
+            return View();
+        }
+    }
+}
