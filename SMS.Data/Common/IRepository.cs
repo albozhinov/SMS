429317e5ee722d9c9f118b2b0b7962bbf6dc39dd
albozhinov <a.bozhinov90@gmail.com>
2022-02-16 20:46:48 +0200

    Repository added

---
+++ SMS.Data/Common/IRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace SMS.Data.Common
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> All();
+
+        void Add(T entity);
+
+        Task AddAsync(T entity);
+
+        void Update(T entity);
+
+        void Save();
+
+        Task SaveAsync();
+    }
+}
---
+++ SMS.Data/Common/Repository.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace SMS.Data.Common
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly SMSDbContext dbContext;
+
+        public Repository(SMSDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        public void Add(T entity)
+        {
+            EntityEntry entry = dbContext.Entry(entity);
+
+            if (entry.State != EntityState.Detached)
+            {
+                entry.State = EntityState.Added;
+            }
+            else
+            {
+                dbContext.Set<T>().Add(entity);
+            }
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            EntityEntry entry = dbContext.Entry(entity);
+
+            if (entry.State != EntityState.Detached)
+            {
+                entry.State = EntityState.Added;
+            }
+            else
+            {
+                await dbContext.Set<T>().AddAsync(entity);
+            }
+        }
+
+        public IQueryable<T> All()
+        {
+            return dbContext.Set<T>().AsQueryable();
+        }
+
+        public void Save()
+        {
+            dbContext.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await dbContext.SaveChangesAsync();
+        }
+
+        public void Update(T entity)
+        {
+            EntityEntry entry = dbContext.Entry(entity);
+
+            if (entry.State == EntityState.Detached)
+            {
+                dbContext.Set<T>().Attach(entity);
+            }
+            
+            entry.State = EntityState.Modified;
+        }
+    }
+}
